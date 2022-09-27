d34f8e7248c34c0497ba077711d2faeedb9e01b6
Joan Puigcerver <joapuiib@gmail.com>
2022-09-27 15:52:51 +0200

    ud1: exemples

---
+++ ud1/examples/CreateWait.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CreateWait {
+    public static void main(String[] args) {
+        if(args.length <= 0) {
+            System.err.println("Es necesita un programa a que executar.");
+            System.exit(-1);
+        }
+
+        ProcessBuilder pb = new ProcessBuilder(args);
+        try {
+            Process process = pb.start();
+            int returnCode = process.waitFor();
+            System.out.println(
+              "L'execució de " + Arrays.toString(args)
+              + " retorna " + returnCode
+            );
+        } catch (IOException ex) {
+            System.err.println("Excepció d'E/S");
+            System.exit(-1);
+        } catch (InterruptedException ex) {
+            System.err.println("El procés fill ha finalizat de manera incorrecta.");
+        }
+    } 
+}
+
---
+++ ud1/examples/RunProcess.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class RunProcess {
+    public static void main (String[] args) throws IOException {
+        if(args.length <= 0) {
+            System.err.println("Cal especificar programa.");
+            System.exit(-1);
+        }
+
+        ProcessBuilder pb = new ProcessBuilder(args);
+        try {
+            Process process = pb.start();
+            int codiRetorn = process.waitFor();
+            System.out.println("L'execució de "+ Arrays.toString(args) +" retorna "+ codiRetorn);
+        } catch (IOException ex) {
+            System.err.println("Excepció d'E/S.");
+            System.exit(-1);
+        } catch (InterruptedException ex) {
+            System.err.println("El procés fill ha finalitzat de manera incorrecta.");
+            System.exit(-1);
+        }
+    }
+}
