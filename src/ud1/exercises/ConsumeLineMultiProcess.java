ee6fea671a47623fcd145912171ad9a3d5ba1b10
Joan Puigcerver <joapuiib@gmail.com>
2022-10-25 17:08:32 +0200

    Fix ConsumeLineMultiProcess

--- src/ud1/exercises/ConsumeLineMultiProcess.java
+++ src/ud1/exercises/ConsumeLineMultiProcess.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 public class ConsumeLineMultiProcess {
     public static void main(String[] args) {
         ArrayList<ProcessBuilder> builders = new ArrayList<>();
-        builders.add(new ProcessBuilder("wsl.exe", "files/consume_line.sh", "files/colors.txt", "3").inheritIO());
-        builders.add(new ProcessBuilder("wsl.exe", "files/consume_line.sh", "files/concessionari.csv", "1").inheritIO());
-        builders.add(new ProcessBuilder("wsl.exe", "files/consume_line.sh", "files/lorem.txt", "2").inheritIO());
+        builders.add(new ProcessBuilder("files/ud1/consume_line.sh", "files/ud1/colors.txt", "3").inheritIO());
+        builders.add(new ProcessBuilder("files/ud1/consume_line.sh", "files/ud1/concessionari.csv", "1").inheritIO());
+        builders.add(new ProcessBuilder("files/ud1/consume_line.sh", "files/ud1/lorem.txt", "2").inheritIO());
 
         ArrayList<Process> processes = new ArrayList<>();
         try {
@@ -21,7 +21,7 @@ public class ConsumeLineMultiProcess {
             System.out.println("Els 3 processos han acabat.");
         } catch (IOException ex) {
             System.err.println("Excepció d'E/S.");
-            System.out.println(ex.getMessage());
+            System.err.println(ex.getMessage());
             System.exit(-1);
         } catch (InterruptedException ex) {
             System.err.println("El procés fill ha finalitzat de manera incorrecta.");
